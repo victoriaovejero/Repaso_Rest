60c296c862e8f238340bbd4def25a9f3f938039f
victoriaovejero <caigamonosmalgmail.com>
2022-04-11 13:47:48 +0200

    dockerization

--- src/main/java/com/example/demo/controller/ClienteController.java
+++ src/main/java/com/example/demo/controller/ClienteController.java
@@ -9,12 +9,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.interfaces.IClienteService;
 import com.example.demo.model.Cliente;
 
 @RestController
+@RequestMapping
 public class ClienteController {
 
 	@Autowired
