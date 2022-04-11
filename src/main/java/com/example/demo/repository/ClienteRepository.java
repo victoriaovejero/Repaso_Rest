401504c8a850d77d272da85eeb8349e0a45835df
victoriaovejero <caigamonosmalgmail.com>
2022-04-11 11:44:42 +0200

    modelo creado

---
+++ src/main/java/com/example/demo/repository/ClienteRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.model.Cliente;
+
+public interface ClienteRepository extends CrudRepository<Cliente, Integer> {
+
+	
+}
